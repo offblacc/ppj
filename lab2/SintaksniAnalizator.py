0d12db1d86c853b54306393f141b18d0769263ef
offblacc <severluka045@gmail.com>
2022-10-23 01:37:01 +0200

    lab2 progress

--- lab2/SintaksniAnalizator.py
+++ lab2/SintaksniAnalizator.py
@@ -1,27 +1,5 @@
 import sys
 
-"""
-<program> ::= <lista_naredbi>
-<lista_naredbi> ::= <naredba> <lista_naredbi>
-<lista_naredbi> ::= $
-<naredba> ::= <naredba_pridruzivanja>
-<naredba> ::= <za_petlja>
-<naredba_pridruzivanja> ::= IDN OP_PRIDRUZI <E>
-<za_petlja> ::= KR_ZA IDN KR_OD <E> KR_DO <E> <lista_naredbi> KR_AZ
-<E> ::= <T> <E_lista>
-<E_lista> ::= OP_PLUS <E>
-<E_lista> ::= OP_MINUS <E>
-<E_lista> ::= $
-<T> ::= <P> <T_lista>
-<T_lista> ::= OP_PUTA <T>
-<T_lista> ::= OP_DIJELI <T>
-<T_lista> ::= $
-<P> ::= OP_PLUS <P>
-<P> ::= OP_MINUS <P>
-<P> ::= L_ZAGRADA <E> D_ZAGRADA
-<P> ::= IDN
-<P> ::= BROJ
-"""
 
 class SintaksniAnalizator:
     data: list
@@ -30,121 +8,133 @@ class SintaksniAnalizator:
     curr_line: str
 
     def __init__(self, data: list):
+        self.it = None
         self.data = data
         self.indent = 0
         self.output = ""
         self.index = 0
         self.program()
-        print(self.output)
-        
 
     def program(self):
         self.output += "<program>\n"
         self.indent += 1
+        self.it = iter(self.data)
+        self.curr_line = next(self.it, None)
         self.lista_naredbi()
+        print(self.output)
 
     def lista_naredbi(self):
-        self.it = iter(self.data)
-        while True:
-            self.indent = 1
-            self.output += self.indent * " " + "<lista_naredbi>\n"
-            self.indent += 1
-            self.curr_line = next(self.it, None)
-            if (self.curr_line is None or self.curr_line.strip() == ""):
-                self.indent += 1
-                self.output += self.indent * " " + "$\n"
-                self.indent -= 2
-                break
-            self.naredba()
+        self.output += self.indent * " " + "<lista_naredbi>"
+        self.indent += 1
+        if self.curr_line is None or self.curr_line.strip() == "":
+            self.output += '\n' + self.indent * " " + "$"
+            self.indent -= 2
+            return
+        self.naredba()
+        self.lista_naredbi()
 
     def naredba(self) -> None:
-        self.output += self.indent * " " + "<naredba>\n"
+        self.output += "\n" + self.indent * " " + "<naredba>\n"
         self.indent += 1
-        if self.curr_line.strip().split()[0] == "KR_ZA": self.za()
-        elif self.curr_line.strip().split()[0] == "IDN": self.idn()
-
+        if self.curr_line:
+            if self.curr_line.strip().split()[0] == "KR_ZA":
+                self.za()
+            elif self.curr_line.strip().split()[0] == "KR_DO":
+                self.do()
+            elif self.curr_line.strip().split()[0] == "KR_AZ":
+                self.az()
+            elif self.curr_line.strip().split()[0] == "IDN":
+                self.idn()
+        self.indent -= 1
 
     def idn(self) -> None:
         self.output += self.indent * " " + "<naredba_pridruzivanja>\n"
         self.indent += 1
         self.output += self.indent * " " + self.curr_line.strip() + "\n"
         self.curr_line = next(self.it, None)
-        if (self.curr_line is None):
-            print("err kraj")
-            exit(0)
-        if (self.curr_line.strip().split()[0] != "OP_PRIDRUZI"):
+        if self.curr_line.strip().split()[0] != "OP_PRIDRUZI":
             print("err" + self.curr_line)
             exit(0)
         self.output += self.indent * " " + self.curr_line.strip() + "\n"
+        self.curr_line = next(self.it, None)
         self.e()
-        
-
-    def za(self):
-        pass
+        self.indent -= 1
 
     def e(self) -> None:
         self.output += self.indent * " " + "<E>\n"
         self.indent += 1
-        self.curr_line = next(self.it, None)
-        if (self.curr_line is None):
-            print("err kraj")
-            exit(0)
         self.t()
         self.e_lista()
+        self.indent -= 1
 
     def t(self) -> None:
         self.output += self.indent * " " + "<T>\n"
         self.indent += 1
         self.p()
         self.t_lista()
+        self.indent -= 1
 
     def p(self) -> None:
         self.output += self.indent * " " + "<P>\n"
         self.indent += 1
-        if self.curr_line.strip().split()[0] in ["OP_PLUS", "OP_MINUS"]:
+        first_word = self.curr_line.strip().split()[0]
+        if first_word in ["OP_PLUS", "OP_MINUS"]:
             self.output += self.indent * " " + self.curr_line.strip() + "\n"
+            self.curr_line = next(self.it, None)
             self.p()
-        elif self.curr_line.strip().split()[0] == "L_ZAGRADA":
+        elif first_word == "L_ZAGRADA":
             self.output += self.indent * " " + self.curr_line.strip() + "\n"
+            self.curr_line = next(self.it, None)
             self.e()
             self.output += self.indent * " " + self.curr_line.strip() + "\n"
-        elif self.curr_line.strip().split()[0] in ["IDN", "BROJ"]:
+        elif first_word in ["IDN", "BROJ"]:
             self.output += self.indent * " " + self.curr_line.strip() + "\n"
-            self.indent -= 1
+        self.indent -= 1
+        self.curr_line = next(self.it, None)
 
     def t_lista(self) -> None:
         self.output += self.indent * " " + "<T_lista>\n"
         self.indent += 1
-        if self.curr_line.strip().split()[0] in ["OP_PUTA", "OP_DIJELI"]:
+        if self.curr_line and self.curr_line.strip().split()[0] in ["OP_PUTA", "OP_DIJELI"]:
             self.output += self.indent * " " + self.curr_line.strip() + "\n"
+            self.curr_line = next(self.it)
             self.t()
         else:
             self.output += self.indent * " " + "$\n"
-            self.indent -=2
+        self.indent -= 1
 
     def e_lista(self) -> None:
         self.output += self.indent * " " + "<E_lista>\n"
         self.indent += 1
-        if self.curr_line.strip().split()[0] in ["OP_PLUS", "OP_MINUS"]:
+        if self.curr_line and self.curr_line.strip().split()[0] in ["OP_PLUS", "OP_MINUS"]:
             self.output += self.indent * " " + self.curr_line.strip() + "\n"
+            self.curr_line = next(self.it, None)
             self.e()
         else:
             self.output += self.indent * " " + "$\n"
-            self.indent -=2
+        self.indent -= 1
 
+    # KR_ZA IDN KR_OD <E> KR_DO <E> <lista_naredbi> KR_AZ
+    def za(self):
+        self.output += self.indent * " " + "<za_petlja>\n"
+        self.indent += 1
+        # idn
+        self.output += self.indent * " " + self.curr_line.strip() + "\n"
+        self.curr_line = next(self.it, None)
+        # od
+        self.output += self.indent * " " + self.curr_line.strip() + "\n"
+        self.curr_line = next(self.it, None)
+        self.e()
+        self.lista_naredbi() # how to now process az
+        self.indent -= 1
 
+    def do(self):
+        pass
 
+    def az(self):
+        pass
 
-
-"""
-<P> ::= OP_PLUS <P>
-<P> ::= OP_MINUS <P>
-<P> ::= L_ZAGRADA <E> D_ZAGRADA
-<P> ::= IDN
-<P> ::= BROJ
-"""
 
 
 if __name__ == "__main__":
-    data = sys.stdin.readlines()
-    SintaksniAnalizator(data)
+    SintaksniAnalizator(sys.stdin.readlines())
