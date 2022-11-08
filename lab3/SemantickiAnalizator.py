52c89661ea2f9fa03ae9ea444cb8d55fb1898335
offblacc <severluka045@gmail.com>
2022-11-08 16:48:23 +0100

    lab3 should be done

--- lab3/SemantickiAnalizator.py
+++ lab3/SemantickiAnalizator.py
@@ -1,27 +1,35 @@
 import sys
 
+
 def main():
-    blstack = [[]] # short for block stack, as in every new block of code adds a list to the stack
-    output = ""
-    curr_line = 0
-    prev_line = -1
-    for line in sys.stdin:
-        line = line.strip().split()
+    blstack, output, curr_line, prev_line, za_declaration, circ_def_chk = [[]], "", 0, -1, False, None
+    for line in [x.strip().split() for x in sys.stdin]:
         prev_line = curr_line
-        if line[0] == "OP_ZA":
+        if line[0] == "KR_ZA":
             blstack.append([])
-        elif line[0] == "OP_AZ":
+            za_declaration = True
+        elif line[0] == "KR_AZ":
             blstack.pop()
         elif line[0] == "IDN":
             curr_line = line[1]
-            if curr_line == prev_line:
-                if lol_contains(blstack, line[2]):
-                    output += line[1] + " " + get_row_from_2d_list(blstack, line[2]) + " "  + line[2] + '\n'
-            else:
-                blstack[-1].append((line[1], line[2]))
-
-    print(blstack)
+            if circ_def_chk is not None and circ_def_chk[1] != curr_line: circ_def_chk = None
+            if za_declaration:
+                blstack[-1].append((curr_line, line[2]))
+                circ_def_chk, za_declaration = (line[2], curr_line), False
+            elif curr_line != prev_line and not lol_contains(blstack, line[2]):
+                blstack[-1].append((curr_line, line[2]))
+                circ_def_chk = (line[2], curr_line)
+            elif curr_line == prev_line and lol_contains(blstack, line[2]):
+                if circ_def_chk is not None and circ_def_chk[0] == line[2]:
+                    output += f"err {curr_line} {line[2]}"
+                    break
+                else:
+                    output += f"{curr_line} {get_row_from_2d_list(blstack, line[2])} {line[2]}\n"
+            elif not lol_contains(blstack, line[2]):
+                output += f"err {curr_line} {line[2]}"
+                break
     print(output.strip())
+
 
 def lol_contains(l: list, e: any):
     for subl in l:
@@ -29,6 +37,7 @@ def lol_contains(l: list, e: any):
             if elem[1] != e: continue
             return True
     return False
+
 
 def get_row_from_2d_list(l: list, e: any):
     for subl in reversed(l):
@@ -39,4 +48,3 @@ def get_row_from_2d_list(l: list, e: any):
 
 if __name__ == "__main__":
     main()
-
